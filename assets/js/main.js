e97a751a2c48b7ac2f3dae23b2d3d22c57158fbb
lo-cri <criloffredi@gmail.com>
2023-04-24 16:29:24 +0200

    snack 2 e 3 finiti

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,9 +51,61 @@ document.querySelector('.container').innerHTML += `
 // Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+const teams =  [
+    {
+        team: 'Roma',
+        punteggio: 0,
+        fallo: 0,
+    },
+    {
+        team: 'Lazio',
+        punteggio: 0,
+        fallo: 0,
+    },
+    {
+        team: 'Frosinone',
+        punteggio: 0,
+        fallo: 0,
+    },
+    {
+        team: 'Latina',
+        punteggio: 0,
+        fallo: 0,
+    },
+    {
+        team: 'Rieti',
+        punteggio: 0,
+        fallo: 0,
+    },
+];
+
+teams.forEach( ( data ) => {
+    data.punteggio = Math.floor(Math.random() * 100) + 1;
+    data.fallo = Math.floor(Math.random() * 100) + 1;
+});
+
+const teamFallo = teams.map( ( {team, fallo} ) => 
+    ({team, fallo}));
+     
+console.log(teamFallo);
+
+
 // Snack 3
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
 // se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
+
+const arrayNomi = ['Daniela', 'Cristina', 'Emanuele', 'Kaveh', 'Lisa', 'Nikolaj',]
+
+let min = parseInt(prompt(`Inserisci un numero tra 0 e ${arrayNomi.length - 1}`))
+let max = parseInt(prompt(`Inserisci un numero tra 0 e 5`))
+
+let arrayAggiunto = arrayNomi.filter((element, index) => {
+    if(min < index && max > index){
+        return element
+    }
+})
+
+console.log(arrayAggiunto)
